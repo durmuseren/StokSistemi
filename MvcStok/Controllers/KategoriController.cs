b2f7ce241357dadeda51381fedf4c37e72ddd4f7
durmuseren <erendurmuseren06@gmail.com>
2021-02-07 21:54:55 +0300

    MVCStok
    MVC kulanılarak yapılmış bir sistemi

---
+++ MvcStok/Controllers/KategoriController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;//modelsi tanımladık
+using PagedList;
+using PagedList.Mvc;
+
+namespace MvcStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        // GET: Kategori
+        MvcDbStokEntities db = new MvcDbStokEntities(); //db nesne.mvcstokentitiesten türettik.Tablolara ulaşmak için db nesnesine ihtiyacımız var.
+        public ActionResult Index(int sayfa=1) //sayfalama için değişken tanımladık.
+        {
+            //  var degerler = db.TBLKATEGORILER.ToList(); //tbl kategoriler içinde bulunan değerleri bana listele
+            var degerler = db.TBLKATEGORILER.ToList().ToPagedList(sayfa, 4);//KAçıncı sayfadan başlasın 1,kaç tane olsun bir sayfada 4 
+            return View(degerler); //bana geriye degerleri döndür.
+        }
+        [HttpGet] //Sayfada herhangi bir işlem yapılmıyorsa sadece görüünümü geri döndür.
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+        [HttpPost] //sayfa işlem yapıldığında aşağıdaki kodları çalıştır.Mesela butona tıklandığında vs.
+        public ActionResult YeniKategori(TBLKATEGORILER p1) //p1 parametre
+        {
+            if(!ModelState.IsValid)// Doğrulama işlemi yapılmadıysa
+            {
+                return View("YeniKategori");
+            }
+            db.TBLKATEGORILER.Add(p1);//p1den gelen değerleri eklep1in değerleri viewden gelcek
+            db.SaveChanges();
+            return View();
+        }
+        public ActionResult SIL(int id) //silme işlemi.ilişkili kategoridekiler silinmez
+        {
+            var kategori = db.TBLKATEGORILER.Find(id); //kategoriler içinde bul
+            db.TBLKATEGORILER.Remove(kategori);//sil
+            db.SaveChanges();//değişiklikleri kaydet
+            return RedirectToAction("Index");//index sayfasına yönlendir
+        }
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktgr = db.TBLKATEGORILER.Find(id);
+            return View("KategoriGetir", ktgr);//kategori getir içinde bize ktgryi getirsin.
+        }
+        public ActionResult Guncelle(TBLKATEGORILER p1)//parametreye göre güncelliycez
+        {
+            var ktg = db.TBLKATEGORILER.Find(p1.KATEGORIID);//pden göndermiş olduğum kategorinin idsini bul
+            ktg.KATEGORIAD = p1.KATEGORIAD;//
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcStok/Controllers/UrunController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcStok.Models.Entity;
+
+namespace MvcStok.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: Urun
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TBLURUNLER.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult UrunEkle()
+        {
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList() //linq sorgusu burda i değişken
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KATEGORIAD,
+                                                 Value = i.KATEGORIID.ToString()
+                                             }).ToList();
+            ViewBag.dgr = degerler;//viewbag diğer sayfaya verileri taşımaya yarar
+            return View();
+        }
+        [HttpPost]
+        public ActionResult UrunEkle(TBLURUNLER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Where(m => m.KATEGORIID == p1.TBLKATEGORILER.KATEGORIID).FirstOrDefault();//firstordefault seçmiş olduğumuz ilk değeri bize getircek
+            p1.TBLKATEGORILER = ktg;
+            db.TBLURUNLER.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index"); //kaydetme işleminden sonra bizi index sayfasına yönlendirsin
+        }
+        public ActionResult SIL(int id)//idye göre silme yapcaz demek
+        {
+            var urun = db.TBLURUNLER.Find(id);//idleri bulcak
+            db.TBLURUNLER.Remove(urun);//tblürünlerden bulmuş olduğun ürünü kaldır.
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UrunGetir(int id)
+        {
+            var urun = db.TBLURUNLER.Find(id);
+            List<SelectListItem> degerler = (from i in db.TBLKATEGORILER.ToList() //linq sorgusu burda i değişken
+                                             select new SelectListItem
+                                             {
+                                                 Text = i.KATEGORIAD,
+                                                 Value = i.KATEGORIID.ToString()
+                                             }).ToList();
+            ViewBag.dgr = degerler;
+            return View("UrunGetir", urun);
+        }
+        public ActionResult Guncelle(TBLURUNLER p)
+        {
+            var urun = db.TBLURUNLER.Find(p.URUNID);
+            urun.URUNAD = p.URUNAD;
+            urun.MARKA = p.MARKA;
+            urun.STOK = p.STOK;
+            urun.FIYAT = p.FIYAT;
+            //   urun.URUNKATEGORI = p.URUNKATEGORI;
+            var ktg = db.TBLKATEGORILER.Where(m => m.KATEGORIID == p.TBLKATEGORILER.KATEGORIID).FirstOrDefault();//firstordefault seçmiş olduğumuz ilk değeri bize getircek
+            urun.URUNKATEGORI = ktg.KATEGORIID; //kategoriyi güncellemek için bu ve üstteki satırı ekledik.
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
